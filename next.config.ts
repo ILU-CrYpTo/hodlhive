7e3d18a1fedec0e8936c00bed295f660d565aab0
ILU-CrYpTo <timuraridi@gmail.com>
2025-02-15 00:48:28 +0300

    upd

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath: isProd ? '/dapp' : '',
-  assetPrefix: isProd ? '/dapp/' : '',
   images: {
     unoptimized: true,
   },
