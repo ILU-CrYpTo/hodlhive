399eda921e6f2c5ea78d8d39fa6f67ef5ca55233
ILU-CrYpTo <timuraridi@gmail.com>
2025-04-02 03:16:40 +0300

    dvbdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,30 @@
-const MaintenancePage = () => {
+import React from 'react';
+
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+});
+
+export default function HomePage() {
   return (
     <div 
+      className={inter.className}
       style={{
         backgroundColor: '#18181D',
         minHeight: '100vh',
-        width: '100%',
+        width: '100vw',
+        position: 'fixed',
+        top: 0,
+        left: 0,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         margin: 0,
         padding: 0,
-        boxSizing: 'border-box',
+        overflow: 'hidden',
       }}
     >
       <h1 style={{ 
@@ -18,12 +32,12 @@ const MaintenancePage = () => {
           display: 'flex',
           gap: '0.5rem',
           margin: 0,
+          lineHeight: 1,
           padding: '1rem',
-          boxSizing: 'border-box'
         }}>
         <span style={{ color: '#FFFFFF', fontWeight: 400 }}>under</span>
         <span style={{ color: '#F7D000', fontWeight: 700 }}>maintenance</span>
       </h1>
     </div>
   );
-};
+}
