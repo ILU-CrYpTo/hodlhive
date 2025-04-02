312d9aef971e2258dcc8f78e7e6ca2002f76ace9
ILU-CrYpTo <timuraridi@gmail.com>
2025-04-02 03:27:33 +0300

    sdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Head from 'next/head'; // Импортируем компонент Head
 import { Inter } from 'next/font/google';
 
 const inter = Inter({
@@ -10,34 +10,42 @@ const inter = Inter({
 
 export default function HomePage() {
   return (
-    <div 
-      className={inter.className}
-      style={{
-        backgroundColor: '#18181D',
-        minHeight: '100vh',
-        width: '100vw',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 0,
-        padding: 0,
-        overflow: 'hidden',
-      }}
-    >
-      <h1 style={{ 
-          fontSize: 'clamp(2rem, 5vw, 3rem)', 
+    <>
+      {/* Добавляем секцию с метаданными */}
+      <Head>
+        <title>HodlHive</title> {/* Заголовок вкладки */}
+        <link rel="icon" href="/favicon.ico" /> {/* Путь к иконке */}
+      </Head>
+
+      <div 
+        className={inter.className}
+        style={{
+          backgroundColor: '#18181D',
+          minHeight: '100vh',
+          width: '100vw',
+          position: 'fixed',
+          top: 0,
+          left: 0,
           display: 'flex',
-          gap: '0.5rem',
+          justifyContent: 'center',
+          alignItems: 'center',
           margin: 0,
-          lineHeight: 1,
-          padding: '1rem',
-        }}>
-        <span style={{ color: '#FFFFFF', fontWeight: 400 }}>under</span>
-        <span style={{ color: '#F7D000', fontWeight: 700 }}>maintenance</span>
-      </h1>
-    </div>
+          padding: 0,
+          overflow: 'hidden',
+        }}
+      >
+        <h1 style={{ 
+            fontSize: 'clamp(2rem, 5vw, 3rem)', 
+            display: 'flex',
+            gap: '0.5rem',
+            margin: 0,
+            lineHeight: 1,
+            padding: '1rem',
+          }}>
+          <span style={{ color: '#FFFFFF', fontWeight: 400 }}>under</span>
+          <span style={{ color: '#F7D000', fontWeight: 700 }}>maintenance</span>
+        </h1>
+      </div>
+    </>
   );
 }
